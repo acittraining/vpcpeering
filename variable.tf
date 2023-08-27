400364d8b4027b589beaf3791399975352c32ac6
JerryBame <106651265+acittraining@users.noreply.github.com>
2023-08-27 13:40:05 +0200

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable list {
+  default = [ "requester_vpc", "accepter_vpc"]
+}
+
+
+variable map {
+  default = {
+    requester_vpc = "10.0.0.0/18"
+    accepter_vpc = "10.1.0.0/18"
+    
+  }
+}
